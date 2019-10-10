496934e2e9f95e29b3ecdc68d19efb704b81e40c
Mohammad Imran Ansari <Imranonline@users.noreply.github.com>
2019-10-10 14:08:28 +0530

    create vpc using terraform

---
+++ vpc/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "${var.aws_region}"
+  access_key = "PUT YOUR AWS ACCESS KEY"
+  secret_key = "PUT YOUR AWS SECRET KEY"
+}
---
+++ vpc/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+	default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+	default = "10.20.0.0/16"
+}
+
+variable "subnets_cidr" {
+	type = "list"
+	default = ["10.20.1.0/24", "10.20.2.0/24"]
+}
+
+variable "azs" {
+	type = "list"
+	default = ["us-east-1a", "us-east-1b"]
+}
